a3886ac54568bc3eba8c9ca215e4214ef684a657
Lovrenc Avsenek <a.lovrenc@gmail.com>
2023-08-01 18:39:51 +0200

    Check if paths are set before templating

--- templates/custom-scan.sh
+++ templates/custom-scan.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+{% if clamav_scan_paths %}
 {% for scan_path in clamav_scan_paths|default(omit) %}
 /usr/bin/clamdscan --multiscan --fdpass --infected  {{ scan_path }}
 {% endfor %}
+{% endif %}
